a8bfef49f06879f920d1737c408da45d5da57639
larskris <wondringaloud@hotmail.com>
2015-09-29 09:40:10 -0700

    Added a city array and randomness, previous users and posts are destroyed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,17 +6,28 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-5.times do 
+# Clear Database
+User.destroy_all
+Post.destroy_all
+
+# San Francisco twice to increase chanses of it being picked
+cities = ["San Francisco", "San Francisco", "New York", "Los Angeles", "Boulder", "Chicago", "Las Vegas"]
+
+# Creates 20 users
+20.times do 
 	email = FFaker::Internet.email
 	first_name = FFaker::Name.first_name
 	last_name = FFaker::Name.last_name
-	current_city = FFaker::AddressUS.city
+	current_city = cities[rand(cities.length)] #FFaker::AddressUS.city
+	# Each seeded user has password "qwe"
 	user_data = {email: email, password: "qwe", first_name: first_name, last_name: last_name, current_city: current_city}
 	user = User.create(user_data)
-	5.times do
+# Creates 5 - 15 posts for each user
+	(rand(11) + 5).times do
 		title = FFaker::Movie.title
 		content = FFaker::BaconIpsum.paragraph
-		city = current_city
+		# 20% chance post is the same as their current_city
+		city = rand(10) < 8 ? current_city : "San Francisco" 
 		user_id = user.id 
 		post_data = {title: title, content: content, city: city, user_id: user_id}
 		Post.create(post_data)
