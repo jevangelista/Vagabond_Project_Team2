d04549d317658dcfa1e0b0a17bb109a46eca8717
Jamie Eula Evangelista <jevangelista@Jamies-MacBook-Pro-2.local>
2015-09-30 11:37:56 -0700

    fixing posts#show route per Nathan's suggestion

--- config/routes.rb
+++ config/routes.rb
@@ -45,7 +45,7 @@ Rails.application.routes.draw do
   patch "/users/:id", to: "users#update" 
 
 
-  get 'users/posts/:id' => 'users#posts', as: "user_posts"
+  get 'users/posts/:id', to: 'users#posts', as: "user_posts"
 
 
  
